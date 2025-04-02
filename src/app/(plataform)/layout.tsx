51a75e7a09cf7fe50b3ab1ca9ab53003f043f043
isma <51026438+ismaelherreradev@users.noreply.github.com>
2025-04-02 13:40:47 -0400

    update shadcn and clerk auth

--- src/app/(plataform)/layout.tsx
+++ src/app/(plataform)/layout.tsx
@@ -4,10 +4,8 @@ import { HydrateClient } from "~/trpc/server";
 
 import { Navbar } from "./_components/navbar";
 
-export default async function DashboardLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  const { orgId } = auth();
+export default async function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const { orgId } = await auth();
 
   if (!orgId) {
     return redirect("/select-org");
@@ -15,7 +13,7 @@ export default async function DashboardLayout({
 
   return (
     <HydrateClient>
-      <main className="min-h-svh container">
+      <main className="min-h-svh mx-auto container">
         <Navbar orgId={orgId} />
         {children}
       </main>
